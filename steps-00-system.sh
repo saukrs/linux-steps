27792b5978cb39fe9c6641d3426b95177a19d0ae
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-04 16:07:40 +0200

    steps-00-system.sh: Track changes in /etc

--- steps-00-system.sh
+++ steps-00-system.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+sudo apt install -y etckeeper           # Log the /etc changes
+
 sudo apt install -y vim
 sudo apt install -y htop iotop-c
 sudo apt install -y traceroute          # for TCP tracing
