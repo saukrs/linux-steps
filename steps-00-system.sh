57aad5ba939d55f574eeacc5c48b22cb70f9f6af
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-04 16:07:09 +0200

    steps-00-system.sh: Get essential tools

--- steps-00-system.sh
+++ steps-00-system.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env sh
+
+sudo apt install -y vim
+sudo apt install -y htop iotop-c
+sudo apt install -y traceroute          # for TCP tracing
+sudo apt install -y asciinema
+sudo apt install -y git
