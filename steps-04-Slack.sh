57bf3c1c786de3719a559de6f278a1dba9373deb
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-07 16:47:25 +0200

    steps-04-Slack.sh: The initial download try

--- steps-04-Slack.sh
+++ steps-04-Slack.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
 # 2022-11-07 saukrs: https://downloads.slack-edge.com/releases/linux/4.28.184/prod/x64/slack-desktop-4.28.184-amd64.deb
+
+curl https://slack.com/intl/en-gb/downloads/instructions/ubuntu | elinks -dump | awk '/deb$/ {print $NF}' | xargs curl -N -# -O
