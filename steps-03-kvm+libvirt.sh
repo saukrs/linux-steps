b6ce3115b6ca8e2a5b92239b822dc0e55b86a60d
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-07 12:46:37 +0200

    steps-03-kvm+libvirt.sh: The setup part

--- steps-03-kvm+libvirt.sh
+++ steps-03-kvm+libvirt.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env sh
+
+# Via: https://artofcode.wordpress.com/2021/02/04/how-to-install-kvm-qemu-on-linux-mint-20-1/
+
+sudo apt-get -y install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
+sudo adduser $USER libvirt
+sudo adduser $USER kvm
+sudo adduser $USER libvirt-qemu
