cc7cba1e3406953ba68cd65b85a57cdb6dfb3b1c
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-07 16:51:12 +0200

    steps-04-Slack.sh: URL in the var

--- steps-04-Slack.sh
+++ steps-04-Slack.sh
@@ -2,4 +2,6 @@
 
 # 2022-11-07 saukrs: https://downloads.slack-edge.com/releases/linux/4.28.184/prod/x64/slack-desktop-4.28.184-amd64.deb
 
-curl https://slack.com/intl/en-gb/downloads/instructions/ubuntu | elinks -dump | awk '/deb$/ {print $NF}' | xargs curl -N -# -O
+DOWNLOAD_URLS_PAGE="https://slack.com/intl/en-gb/downloads/instructions/ubuntu"
+
+curl $DOWNLOAD_URLS_PAGE | elinks -dump | awk '/deb$/ {print $NF}' | xargs curl -N -# -O
