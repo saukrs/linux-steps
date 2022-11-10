c165a20badf38b321de6ef9c97e807d81566c932
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-10 16:14:20 +0200

    steps-05-ctop.sh: A first shot

--- steps-05-ctop.sh
+++ steps-05-ctop.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env sh
+
+# 2022-11-10 saukrs;
+# Via: https://github.com/bcicen/ctop#debianubuntu
+
+sudo apt-get install ca-certificates curl gnupg lsb-release
+curl -fsSL https://azlux.fr/repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/azlux-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian \
+  stable main" | sudo tee /etc/apt/sources.list.d/azlux.list
+sudo apt-get update
+sudo apt-get install docker-ctop
