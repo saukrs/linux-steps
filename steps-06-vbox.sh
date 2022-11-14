3a49ea61a1f5992f5914836763f36395be4df638
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-14 10:26:37 +0200

    steps-06-vbox.sh: Steps from `virtualbox.org` download page

--- steps-06-vbox.sh
+++ steps-06-vbox.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env sh
+
+curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
+
+. /etc/os-release
+
+cat << \
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian $UBUNTU_CODENAME contrib
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+sudo tee /etc/apt/sources.list.d/vbox.list
+
+sudo apt-get update
+sudo apt-get -y install virtualbox-7.0
