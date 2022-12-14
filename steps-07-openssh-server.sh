90d603da0fab4761a8c3cafcbf5a5ca1e179cf51
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-12-14 23:11:33 +0200

    steps-07-openssh-server.sh: Copy the pubkey to the root too

--- steps-07-openssh-server.sh
+++ steps-07-openssh-server.sh
@@ -4,3 +4,5 @@ sudo apt install -y openssh-server
 ssh-copy-id localhost   # An interactive auth
 echo "Running commands via SSH:"
 ssh localhost "hostname; id; uptime"
+
+cd; echo ".ssh\n.ssh/authorized_keys" | cpio -o | sudo sh -c 'cd; cpio --no-preserve-owner -iv'
