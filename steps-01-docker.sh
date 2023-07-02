64658803293e839302e4bbab6b486b24932588f0
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-07-02 20:19:14 +0300

    steps-01-docker.sh: Do not ask questions on the 1st update

--- steps-01-docker.sh
+++ steps-01-docker.sh
@@ -7,7 +7,7 @@ set -e
 # sudo apt-get remove docker docker-engine docker.io containerd runc
 
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
