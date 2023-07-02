ae97b56d0e2625df83c77d1dd28dfc5439ab5549
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-07-02 20:44:30 +0300

    `steps-08-webmin.sh`: Add the reference

--- steps-08-webmin.sh
+++ steps-08-webmin.sh
@@ -4,3 +4,4 @@
 # SPDX-FileCopyrightText: 2023 Saulius Krasuckas <saulius2_at_ar-fi_point_lt> | sskras
 
 # Set up Webmin (using the official repo).
+# Via: https://doxfer.webmin.com/Webmin/Installation#apt_.28Debian.2FUbuntu.2FMint.29
