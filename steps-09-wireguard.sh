5f1366628b7b7d9935e6db0499f4036552ff8274
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-07-07 06:03:38 +0000

    `steps-09-wireguard.sh`: Add the reference

--- steps-09-wireguard.sh
+++ steps-09-wireguard.sh
@@ -4,3 +4,4 @@
 # SPDX-FileCopyrightText: 2023 Saulius Krasuckas <saulius2_at_ar-fi_point_lt> | sskras
 
 # Set up WireGuard server peer.
+# Via: https://www.digitalocean.com/community/tutorials/how-to-set-up-wireguard-on-ubuntu-20-04
