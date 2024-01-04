e0154e5aaa1ecf68e7391f67779bafe367cec5f3
Saulius Krasuckas <saulius2@ar-fi.lt>
2024-01-04 17:27:42 +0000

    `steps-04-Slack.sh`: Algo still works with v4.35.131

--- steps-04-Slack.sh
+++ steps-04-Slack.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 # 2022-11-07 saukrs: https://downloads.slack-edge.com/releases/linux/4.28.184/prod/x64/slack-desktop-4.28.184-amd64.deb
+# 2024-01-04 saukrs: https://downloads.slack-edge.com/releases/linux/4.35.131/prod/x64/slack-desktop-4.35.131-amd64.deb
 
 DOWNLOAD_URLS_PAGE="https://slack.com/intl/en-gb/downloads/instructions/ubuntu"
 
